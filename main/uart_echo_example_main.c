cf3382d6b2a97bc6bacb3d3c0178610588248fa0
Mats14_83 <Mats14_83@bitbucket.org>
2024-12-06 08:24:43 -0300

    Ultimo commit funcional caudalimetro

--- main/uart_echo_example_main.c
+++ main/uart_echo_example_main.c
@@ -1,11 +1,3 @@
-/* UART Echo Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -30,11 +22,13 @@
 
 #define RST_DWIN_CMD "\x5a\xa5\x07\x82\x00\x04\x55\xaa\x5a\xa5"
 #define SWITCH_PAGE "\x5a\xa5\x07\x82\x00\x84\x5a\x01\x00\x01"
+
 #define FLOW_ACUM_COM "\x0F\x03\x02\x04\x00\x04\x05\x5E"
 #define FLOW_INST_COM "\x0F\x03\x02\x09\x00\x02\x14\x9F"
 #define DATE_COM "\x0F\x03\x02\x0C\x00\x06\x05\x5D"
+#define ALL_DATA "\x0F\x03\x02\x00\x00\x19\x84\x96"
 
-#define LOG_EN 0
+#define LOG_EN
 
 #define RX_BUF_SIZE 1024
 #define TX_BUF_SIZE 1024
@@ -49,6 +43,15 @@
 
 #define TAG "UART"
 
+int j;
+
+typedef enum
+{
+    DATE_COM_STATE,
+    FLOW_INST_COM_STATE,
+    FLOW_ACUM_COM_STATE,
+} comm_state_t;
+
 QueueHandle_t uart_queue;
 
 bool once = 0;
@@ -57,12 +60,88 @@ char addr_low, addr_hi, cmd;
 uint16_t addr = 0;
 uint8_t n_bytes;
 uint64_t combined_value = 0;
+uint32_t flow_result = 0;
 int len;
 float acum;
 
+static comm_state_t comm_state = FLOW_ACUM_COM_STATE;
+
 char incoming_message[RX_BUF_SIZE];
+char message[9] = FLOW_ACUM_COM;
 
 void print_data(char *, uint8_t);
+
+esp_err_t send_command(uint8_t);
+
+esp_err_t rcv_command(uint8_t);
+
+esp_err_t calc_flow(int, int);
+
+esp_err_t proccess_command();
+
+/**
+ * @brief Recibe mensajes devueltos desde el caudalimetro
+ * @param [in] n_bytes Numero de bytes para mandar a funcion print_data
+ * @return esp_err_t Estado de la operacion (ESP_OK si todo fue bien, ESP_FAIL si hubo algun problema)
+ */
+
+esp_err_t rcv_command(uint8_t n_bytes)
+{
+#ifdef LOG_EN
+    ESP_LOGI("FUNCION DE RECEPCION", "FUNCION DE RECEPCION\n");
+#endif
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    gpio_set_level(PIN_DE, 0);
+    memset(incoming_message, 0, sizeof(incoming_message));
+    len = uart_read_bytes(ECHO_UART_PORT_NUM, (uint8_t *)incoming_message, RX_BUF_SIZE, pdMS_TO_TICKS(500));
+#ifdef LOG_EN
+    print_data(incoming_message, len);
+#endif
+    return ESP_OK;
+}
+
+/**
+ * @brief Manda los comandos al caudalimetro
+ * @param [in] cmd Comando a enviar al caudalimetro por puerto serie
+ * @param [in] n_bytes Numero de bytes para mandar a funcion print_data
+ * @return esp_err_t Estado de la operacion (ESP_OK si todo fue bien, ESP_FAIL si hubo algun problema)
+ */
+
+esp_err_t send_command(uint8_t n_bytes)
+{
+    gpio_set_level(PIN_DE, 1);
+    uart_write_bytes(ECHO_UART_PORT_NUM, message, (sizeof(message) - 1));
+#ifdef LOG_EN
+    print_data(message, n_bytes);
+#endif
+    ESP_ERROR_CHECK(rcv_command(n_bytes));
+    return ESP_OK;
+}
+
+/**
+ * @brief
+ *
+ */
+
+esp_err_t calc_flow(int first, int last)
+{
+    combined_value = 0;
+    for (int i = first; i < last; i++)
+    {
+        if (comm_state == FLOW_ACUM_COM_STATE)
+            combined_value = (combined_value << 8) | incoming_message[i];
+        if (comm_state == FLOW_INST_COM_STATE)
+            flow_result |= ((uint32_t)incoming_message[i] << ((3 - i + first) * 8));
+    }
+    return ESP_OK;
+}
+
+/**
+ * @brief Tarea para manejo del puerto serie
+ * @param [in] params los parametros de la Task creada
+ * @return void
+ */
 
 void uart_event_task(void *params)
 {
@@ -78,55 +157,58 @@ void uart_event_task(void *params)
             case UART_DATA:
                 uart_read_bytes(ECHO_UART_PORT_NUM, (uint8_t *)incoming_message, RX_BUF_SIZE, pdMS_TO_TICKS(500));
                 n_bytes = incoming_message[3];
+#ifndef RS485
                 addr_hi = incoming_message[4];
                 addr_low = incoming_message[5];
                 addr = addr_low | addr_hi << 8;
-                // printf("0x%02X ", n_bytes);
-                // printf("0x%02X ", incoming_message[4]);
-                // printf("0x%02X ", incoming_message[5]);
-                // printf("0x%04X ", addr);
-                // printf("0x%02X ", incoming_message[n_bytes + 2]);
-                // printf("\n");
-                // ESP_LOGI(TAG, "UART_DATA");
-                // printf("\n");
-                // print_data(incoming_message, n_bytes);
-                // printf("0x%02X ", n_bytes);
-                // printf("\n");
+                printf("0x%02X ", n_bytes);
+                printf("0x%02X ", incoming_message[4]);
+                printf("0x%02X ", incoming_message[5]);
+                printf("0x%04X ", addr);
+                printf("0x%02X ", incoming_message[n_bytes + 2]);
+                printf("\n");
+                ESP_LOGI(TAG, "UART_DATA");
+                printf("\n");
+                print_data(incoming_message, n_bytes);
+                printf("0x%02X ", n_bytes);
+                printf("\n");
+#endif
 
-                for (int i = 4; i < 12; i++)
+                // pasar a funcion:
+                switch (comm_state)
                 {
-                    // printf("0x%02X ", incoming_message[i]);
-                    combined_value |= ((uint64_t)incoming_message[i] << ((7 - i) * 8));
+                case FLOW_ACUM_COM_STATE:
+                    break;
+                case FLOW_INST_COM_STATE:
+                    break;
+                case DATE_COM_STATE:
+                    break;
                 }
-                // printf("\n");
-                // printf("acum: 0x%016llx\n", combined_value);
-                // printf("acum: %lld\n", combined_value);
-                printf("acum: %f\n", (float)combined_value / 1000000);
-                // switch (addr_hi)
-                // {
-                // case /* constant-expression */:
-                //     /* code */
-                //     break;
 
-                // default:
-                //     break;
-                // }
                 break;
             case UART_BREAK:
-                // ESP_LOGI(TAG, "UART_BREAK");
+#ifdef LOG_EN
+                ESP_LOGI(TAG, "UART_BREAK");
+#endif
                 break;
             case UART_BUFFER_FULL:
+#ifdef LOG_EN
                 ESP_LOGI(TAG, "UART_BUFFER_FULL");
+#endif
                 break;
             case UART_FIFO_OVF:
+#ifdef LOG_EN
                 ESP_LOGI(TAG, "UART_FIFO_OVF");
+#endif
                 uart_flush_input(UART_NUM_1);
                 xQueueReset(uart_queue);
                 break;
             case UART_FRAME_ERR:
+
                 ESP_LOGI(TAG, "UART_FRAME_ERR");
                 break;
             case UART_PARITY_ERR:
+
                 ESP_LOGI(TAG, "UART_PARITY_ERR");
                 break;
             case UART_DATA_BREAK:
@@ -150,6 +232,12 @@ void uart_event_task(void *params)
     }
 }
 
+/**
+ * @brief Inicializa entradas y salidas
+ * @param [in] void
+ * @return void
+ */
+
 void init_gpio(void)
 {
 #ifdef RS485
@@ -159,9 +247,16 @@ void init_gpio(void)
     gpio_pullup_en(PIN_SWITCH);
 }
 
+/**
+ * @brief imprime al terminal lo que envia y recibe del caudalimetro (en formato hexadecimal)
+ * @param [in] message Mensaje a imprimir en terminal
+ * @param [in] n_frame Cantidad de bytes a mandar
+ * @return void
+ */
+
 void print_data(char *message, uint8_t n_frame)
 {
-    for (int i = 0; i < (n_frame + 6); i++)
+    for (int i = 0; i < (n_frame); i++)
     {
         printf("0x%X ", message[i]);
     }
@@ -173,9 +268,11 @@ void app_main(void)
     init_gpio();
 
     uart_config_t uart_config = {
-        .baud_rate = ECHO_UART_BAUD_RATE,
+        //.baud_rate = ECHO_UART_BAUD_RATE,
+        .baud_rate = 2400,
         .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
+        //.parity = UART_PARITY_DISABLE,
+        .parity = UART_PARITY_EVEN,
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
     uart_param_config(ECHO_UART_PORT_NUM, &uart_config);
@@ -195,7 +292,6 @@ void app_main(void)
     // printf("received: 0x%X\n", incoming_message);
     print_data(incoming_message);
 #endif
-    // uart_enable_pattern_det_intr(UART_NUM_1, '+', PATTERN_LEN, 10000, 10, 10);
 
     uart_pattern_queue_reset(UART_NUM_1, 20);
     xTaskCreate(uart_event_task, "uart_event_task", 2048, NULL, 10, NULL);
@@ -203,34 +299,46 @@ void app_main(void)
     while (true)
     {
         level = gpio_get_level(PIN_SWITCH);
-        // if (level && !once)
-        // {
-        //     printf("IN: 1\n");
-        //     once = 1;
-        // }
-//         if (!level && once)
-//         {
+#ifdef IN_ACT
+        if (level && !once)
+        {
+            printf("IN: 1\n");
+            once = 1;
+        }
+        if (!level && once)
+        {
 
-//             printf("IN: 0\n");
-//             once = 0;
+            printf("IN: 0\n");
+            once = 0;
+        }
+#endif
 #ifndef RS485
         char message[] = SWITCH_PAGE;
 #else
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        gpio_set_level(PIN_DE, 1);
-        char message[] = "\x0F\x03\x02\x04\x00\x04\x05\x5E"; // 0F 03 02 04 00 04 05 5E
-#endif
-        uart_write_bytes(ECHO_UART_PORT_NUM, message, (sizeof(message) - 1));
-        // print_data(message, 2);
-        // }
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        gpio_set_level(PIN_DE, 0);
-        memset(incoming_message, 0, sizeof(incoming_message));
-        len = uart_read_bytes(ECHO_UART_PORT_NUM, (uint8_t *)incoming_message, RX_BUF_SIZE, pdMS_TO_TICKS(500));
+        switch (comm_state)
+        {
+        case FLOW_ACUM_COM_STATE:
+            memcpy(message, FLOW_ACUM_COM, sizeof(FLOW_ACUM_COM));
+            ESP_ERROR_CHECK(send_command(8));
+            calc_flow(3, 11);
+            printf("acum: %f\n", (float)combined_value / 1000);
+            comm_state = FLOW_INST_COM_STATE;
+            break;
+        case FLOW_INST_COM_STATE:
+            memcpy(message, FLOW_INST_COM, sizeof(FLOW_INST_COM));
+            ESP_ERROR_CHECK(send_command(8));
+            calc_flow(3, 7);
+            printf("flow: %f\n", (float)flow_result * 16.6 / 1000000);
+            comm_state = DATE_COM_STATE;
+            break;
+        case DATE_COM_STATE:
+            memcpy(message, DATE_COM, sizeof(DATE_COM));
+            ESP_ERROR_CHECK(send_command(8));
+            comm_state = FLOW_ACUM_COM_STATE;
+            break;
+        }
 
-        // else
-        // {
-        //     print_data(incoming_message);
-        // }
+#endif
     }
 }
